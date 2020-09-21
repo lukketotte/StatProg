e047692e8aabada5777fbb86c060875456f10c65
Lukas Arnroth <lukar818@user.uu.se>
2020-09-21 10:56:18 +0200

    adding galaxies.rda

---
+++ R/galaxies.R
@@ -0,0 +1,18 @@
+#' Velocities for 81 Galaxies
+#'
+#' A numeric vector of velocities in km/sec of 81 galaxies divided by 1000 from 6
+#' well-separated conic sections of an unfilled survey of the
+#' Corona Borealis region. Multimodality in such surveys is evidence
+#' for voids and superclusters in the far universe.
+#'
+#'@docType data
+#'
+#'@references Roeder, K. (1990) Density estimation with
+#'confidence sets exemplified by superclusters and voids in galaxies.
+#'Journal of the American Statistical Association 85, 617–624.
+#'
+#'Postman, M., Huchra, J. P. and Geller, M. J. (1986) Probes of large-scale
+#'structures in the Corona Borealis region. Astronomical Journal 92, 1238–1247.
+#'
+#'@usage data(galaxies)
+"galaxies"
