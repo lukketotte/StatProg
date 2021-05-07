da05932c39796c358d810d6df0f5625d77efd4e6
lukketotte <lukas.arnroth@gmail.com>
2021-05-07 16:19:56 +0200

    Lecture 1 done

--- test.R
+++ test.R
@@ -11,3 +11,13 @@ galaxies = MASS::galaxies / 1000
 
 
 StatProg::galaxies
+
+x = matrix(runif(9), 3, 3)
+dim(x)
+
+dim(StatProg::galaxies)
+
+c(TRUE, FALSE) && TRUE
+set.seed(123)
+x = rnorm(100, 0.5)
+t.test(x)
